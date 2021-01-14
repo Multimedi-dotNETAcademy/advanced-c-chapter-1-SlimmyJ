ef2cd8021b1b5e42e3b4126cda05d7bba7691957
Simon Van Tittelboom <simon.van.tittelboom@gmail.com>
2021-01-14 13:16:39 +0100

    Add project files.

---
+++ oefeningenklassikaal/Rommelzinclass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oefeningenklassikaal
+{
+    internal class Rommelzinclass
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Colour { get; set; }
+        public string Movie { get; set; }
+    }
+}
